43a390841c1aa401012276f24edf53f01ccb9d63
ZahidHM <zahidhm.dp@gmail.com>
2025-07-26 03:36:09 -0500

    errase border

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -22,7 +22,7 @@ export default function Dashboard() {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-3 border-1 border-green-500 p-1">
+                <div className="grid auto-rows-min gap-4 md:grid-cols-3  p-1">
                     {/* <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     </div> */}
